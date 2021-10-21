a8a731d04ca4cfe1605d52f362962eb675b6f1ec
awelch17 <awelch17@geffenacademy.ucla.edu>
2021-10-21 10:40:50 -0700

    attempt straight wgpu

--- src/renderer.rs
+++ src/renderer.rs
@@ -10,10 +10,7 @@ struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
-    config: wgpu::SurfaceConfiguration,
-    imgui: Context,
-    renderer: Renderer,
-    last_frame: Instant
+    config: wgpu::SurfaceConfiguration
 }
 
 
@@ -57,103 +54,49 @@ impl ParamRenderer {
         };
         surface.configure(&device, &config);
         
-        let mut imgui = imgui::Context::create();
-        
-        let renderer_config = RendererConfig {
-            ..Default::default()
-        };
-    
-        let mut renderer = Renderer::new(&mut imgui, &device, &queue, renderer_config);
-        
-        
         let state = State {
             surface: surface,
             device: device,
             queue: queue,
-            config: config,
-            imgui: imgui,
-            renderer: renderer,
-            last_frame: Instant::now()
+            config: config
         };
         
         Self { state: state }
     }
     
     pub fn draw_frame(&mut self) {
-        
-        
-        let delta_s = self.state.last_frame.elapsed();
-        let now = Instant::now();
-        self.state.imgui.io_mut().update_delta_time(now - self.state.last_frame);
-        self.state.last_frame = now;
-        
-        let frame = match self.state.surface.get_current_texture() {
-            Ok(frame) => frame,
-            Err(e) => {
-                eprintln!("dropped frame: {:?}", e);
-                return;
+        if let Ok(output) = self.state.surface.get_current_texture() {
+            let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+            
+            let mut encoder = self.state.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+            
+            {
+                let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: Some("Render Pass"),
+                    color_attachments: &[wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(wgpu::Color {
+                                r: 1.0,
+                                g: 0.2,
+                                b: 0.3,
+                                a: 1.0,
+                            }),
+                            store: true,
+                        },
+                    }],
+                    depth_stencil_attachment: None,
+                });
             }
-        };
         
-        let ui = self.state.imgui.frame();
+            // submit will accept anything that implements IntoIter
+            self.state.queue.submit(std::iter::once(encoder.finish()));
+            output.present();
         
-        {
-            let window = imgui::Window::new("Hello world");
-            window
-                .size([WINDOW_DIMENSIONS.0 as f32, WINDOW_DIMENSIONS.1 as f32], Condition::FirstUseEver)
-                .build(&ui, || {
-                    ui.text("Hello world!");
-                    ui.text("This...is...imgui-rs on WGPU!");
-                    ui.separator();
-                    let mouse_pos = ui.io().mouse_pos;
-                    ui.text(format!(
-                        "Mouse Position: ({:.1},{:.1})",
-                        mouse_pos[0], mouse_pos[1]
-                    ));
-                });
-
-            let window = imgui::Window::new("Hello too");
-            window
-                .size([400.0, 200.0], Condition::FirstUseEver)
-                .position([400.0, 200.0], Condition::FirstUseEver)
-                .build(&ui, || {
-                    ui.text(format!("Frametime: {:?}", delta_s));
-                });
-                
-            ui.show_demo_window(&mut true);
-            
-            
         }
         
-        let mut encoder: wgpu::CommandEncoder = self.state.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-        
-        let clear_color = wgpu::Color {
-            r: 0.1,
-            g: 0.2,
-            b: 0.3,
-            a: 1.0,
-        };
-        
-        let view = frame
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: None,
-            color_attachments: &[wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(clear_color),
-                    store: true,
-                },
-            }],
-            depth_stencil_attachment: None,
-        });
-        self.state.renderer
-            .render(ui.render(), &self.state.queue, &self.state.device, &mut rpass)
-            .expect("Rendering failed");
-        drop(rpass);
-        self.state.queue.submit(Some(encoder.finish()));
-        frame.present();
     }
 }
